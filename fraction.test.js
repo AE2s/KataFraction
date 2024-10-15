c18c30ad511a5420e292e64c5fb298345df2aae0
ESSAGHIR Anas (External) <anas.essaghir.ext@loccitane.com>
2024-10-15 17:51:15 +0200

    update all tests with fraction class

--- fraction.test.js
+++ fraction.test.js
@@ -3,18 +3,27 @@ import Fraction from "./fraction";
 const add = (a, b, c, d) => a + c;
 test("0 + 0 equal to 0", () => {
   const fractionZero = new Fraction(0, 1);
-  const addition = fractionZero.add(fractionZero)
+  const addition = fractionZero.add(fractionZero);
   expect(addition).toBe(0);
 });
 
 test("0 + 5 equal to 5", () => {
-  expect(add(0, 1, 5, 1)).toBe(5);
+  const fractionZero = new Fraction(0, 1);
+  const secondFraction = new Fraction(5, 1);
+  const addition = fractionZero.add(secondFraction);
+  expect(addition).toBe(5);
 });
 
 test("6 + 5 equal to 11", () => {
-  expect(add(6, 1, 5, 1)).toBe(11);
+    const firstFraction = new Fraction(6, 1);
+    const secondFraction = new Fraction(5, 1);
+    const addition = firstFraction.add(secondFraction);
+    expect(addition).toBe(11);
 });
 
 test("2/1 + 4/1 equal to 6", () => {
-  expect(add(2, 1, 4, 1)).toBe(6);
+    const firstFraction = new Fraction(2, 1);
+    const secondFraction = new Fraction(4, 1);
+    const addition = firstFraction.add(secondFraction);
+    expect(addition).toBe(6);
 });
