46c7d917a023eaab10daea97d0ce215ff2af9572
ESSAGHIR Anas (External) <anas.essaghir.ext@loccitane.com>
2024-10-16 10:52:43 +0200

    Add test for minus operation and refacto

--- fraction.js
+++ fraction.js
@@ -13,6 +13,14 @@ export default class Fraction {
     return result.simplify().toString();
   };
 
+  minus = (fraction) => {
+    if (this.denominator === 0 || fraction.denominator === 0) return null;
+    const nominator = this.nominator * fraction.denominator - fraction.nominator * this.denominator;
+    const denominator = this.denominator * fraction.denominator;
+    const result = new Fraction(nominator, denominator);
+    return result.simplify().toString();
+  };
+
   simplify = () => {
     const _pgcd = pgcd(this.nominator, this.denominator);
     return new Fraction(this.nominator / _pgcd, this.denominator / _pgcd);
--- fraction.test.js
+++ fraction.test.js
@@ -1,73 +1,104 @@
 import Fraction from "./fraction";
 
-test("0 + 0 equal to 0", () => {
-  const fractionZero = new Fraction(0, 1);
-  const addition = fractionZero.add(fractionZero);
-  expect(addition).toBe("0");
+describe("Addition", () => {
+  test("0 + 0 equal to 0", () => {
+    const fractionZero = new Fraction(0, 1);
+    const addition = fractionZero.add(fractionZero);
+    expect(addition).toBe("0");
+  });
+
+  test("0 + 5 equal to 5", () => {
+    const fractionZero = new Fraction(0, 1);
+    const secondFraction = new Fraction(5, 1);
+    const addition = fractionZero.add(secondFraction);
+    expect(addition).toBe("5");
+  });
+
+  test("6 + 5 equal to 11", () => {
+    const firstFraction = new Fraction(6, 1);
+    const secondFraction = new Fraction(5, 1);
+    const addition = firstFraction.add(secondFraction);
+    expect(addition).toBe("11");
+  });
+
+  test("2/1 + 4/1 equal to 6", () => {
+    const firstFraction = new Fraction(2, 1);
+    const secondFraction = new Fraction(4, 1);
+    const addition = firstFraction.add(secondFraction);
+    expect(addition).toBe("6");
+  });
+
+  test("5/0 + 4/1 should return null", () => {
+    const firstFraction = new Fraction(5, 0);
+    const secondFraction = new Fraction(4, 1);
+    const addition = firstFraction.add(secondFraction);
+    expect(addition).toBe(null);
+  });
+
+  test("4/1 + 5/0 should return null", () => {
+    const firstFraction = new Fraction(5, 0);
+    const secondFraction = new Fraction(4, 1);
+    const addition = secondFraction.add(firstFraction);
+    expect(addition).toBe(null);
+  });
+
+  test("1/2 + 1/2 equal to 1", () => {
+    const firstFraction = new Fraction(1, 2);
+    const secondFraction = new Fraction(1, 2);
+    const addition = firstFraction.add(secondFraction);
+    expect(addition).toBe("1");
+  });
+
+  test("1/3 + 1/2 equal to 5/6", () => {
+    const firstFraction = new Fraction(1, 3);
+    const secondFraction = new Fraction(1, 2);
+    const addition = firstFraction.add(secondFraction);
+    const expected = "5/6";
+    expect(addition).toBe(expected);
+  });
+
+  test("3/5 + 14/45 equal to 41/45", () => {
+    const firstFraction = new Fraction(3, 5);
+    const secondFraction = new Fraction(14, 45);
+    const addition = firstFraction.add(secondFraction);
+    const expected = "41/45";
+    expect(addition).toBe(expected);
+  });
+
+  test("3/6 + 6/18 equal to 5/6", () => {
+    const firstFraction = new Fraction(3, 6);
+    const secondFraction = new Fraction(6, 18);
+    const addition = firstFraction.add(secondFraction);
+    const expected = "5/6";
+    expect(addition).toBe(expected);
+  });
 });
 
-test("0 + 5 equal to 5", () => {
-  const fractionZero = new Fraction(0, 1);
-  const secondFraction = new Fraction(5, 1);
-  const addition = fractionZero.add(secondFraction);
-  expect(addition).toBe("5");
-});
+describe("Soustraction", () => {
+  test("0 - 0 equal to 0", () => {
+    const fractionZero = new Fraction(0, 1);
+    const addition = fractionZero.minus(fractionZero);
+    expect(addition).toBe("0");
+  });
 
-test("6 + 5 equal to 11", () => {
-  const firstFraction = new Fraction(6, 1);
-  const secondFraction = new Fraction(5, 1);
-  const addition = firstFraction.add(secondFraction);
-  expect(addition).toBe("11");
-});
+  test("6 - 0 equal to 6", () => {
+    const firstFraction = new Fraction(6, 1);
+    const secondFraction = new Fraction(0, 1);
+    const addition = firstFraction.minus(secondFraction);
+    expect(addition).toBe("6");
+  });
 
-test("2/1 + 4/1 equal to 6", () => {
-  const firstFraction = new Fraction(2, 1);
-  const secondFraction = new Fraction(4, 1);
-  const addition = firstFraction.add(secondFraction);
-  expect(addition).toBe("6");
-});
+  test("0 - 5 equal to -5", () => {
+    const firstFraction = new Fraction(0, 1);
+    const secondFraction = new Fraction(5, 1);
+    const addition = firstFraction.minus(secondFraction);
+    expect(addition).toBe("-5");
+  });
 
-test("5/0 + 4/1 should return null", () => {
-  const firstFraction = new Fraction(5, 0);
-  const secondFraction = new Fraction(4, 1);
-  const addition = firstFraction.add(secondFraction);
-  expect(addition).toBe(null);
-});
-
-test("4/1 + 5/0 should return null", () => {
-  const firstFraction = new Fraction(5, 0);
-  const secondFraction = new Fraction(4, 1);
-  const addition = secondFraction.add(firstFraction);
-  expect(addition).toBe(null);
-});
-
-test("1/2 + 1/2 equal to 1", () => {
-  const firstFraction = new Fraction(1, 2);
-  const secondFraction = new Fraction(1, 2);
-  const addition = firstFraction.add(secondFraction);
-  expect(addition).toBe("1");
-});
-
-test("1/3 + 1/2 equal to 5/6", () => {
-  const firstFraction = new Fraction(1, 3);
-  const secondFraction = new Fraction(1, 2);
-  const addition = firstFraction.add(secondFraction);
-  const expected = "5/6";
-  expect(addition).toBe(expected);
-});
-
-test("3/5 + 14/45 equal to 41/45", () => {
-  const firstFraction = new Fraction(3, 5);
-  const secondFraction = new Fraction(14, 45);
-  const addition = firstFraction.add(secondFraction);
-  const expected = "41/45";
-  expect(addition).toBe(expected);
-});
-
-test("3/6 + 6/18 equal to 5/6", () => {
-  const firstFraction = new Fraction(3, 6);
-  const secondFraction = new Fraction(6, 18);
-  const addition = firstFraction.add(secondFraction);
-  const expected = "5/6";
-  expect(addition).toBe(expected);
+  test("5/0 - 4/1 should return null", () => {
+    const firstFraction = new Fraction(5, 0);
+    const secondFraction = new Fraction(4, 1);
+    const addition = firstFraction.minus(secondFraction);
+    expect(addition).toBe(null);
+  });
 });
